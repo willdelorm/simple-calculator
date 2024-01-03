0cdec4c7123f18e3ad0490d550570b9b6d3da9bd
Will Delorm <willdelorm@gmail.com>
2024-01-03 11:01:38 -0800

    Calculator functionality rewritten without eval

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
-// Save input variable
-const input = document.getElementById('input');
+const display = document.getElementById("display");
+const buttons = Array.from(document.getElementsByClassName("btn"));
 
-// Save buttons to an array
-const buttons = Array.from(document.getElementsByClassName('btn'));
+let acc = "",
+  currentValue = "",
+  operator = "";
+let isDisplayActive = false;
 
-// Map event listeners to each button and respond to button value
-buttons.map(button => {
-  button.addEventListener('click', (e) => {
-    switch (e.target.innerText) {
-      case 'AC':
-        input.innerText = '';
-        break;
-      case '=':
-        try {
-          // ! eval() is a security risk on user-facing applications
-          input.innerText = eval(input.innerText);
-        } catch {
-          input.innerText = 'Error';
-        }
-        break;
-      case 'C':
-        if (input.innerText) {
-          input.innerText = input.innerText.slice(0, -1);
-        }
-        break;
-      default:
-        input.innerText += e.target.innerText;
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    const keyValue = e.target.innerText;
+    if (Number(keyValue) || keyValue === ".") {
+      if (isDisplayActive) {
+        display.innerText += keyValue;
+      } else {
+        isDisplayActive = true;
+        display.innerText = keyValue;
+      }
+    } else if (keyValue.match(/[\+\-x\/]/)) {
+      isDisplayActive = false;
+      acc = display.innerText;
+      operator = keyValue;
+    } else if (keyValue === "=") {
+      isDisplayActive = false;
+      if (!currentValue) {
+        currentValue = display.innerText;
+      }
+      switch (operator) {
+        case "+":
+          display.innerText = Number(acc) + Number(currentValue);
+          break;
+        case "-":
+          display.innerText = Number(acc) - Number(currentValue);
+          break;
+        case "x":
+          display.innerText = Number(acc) * Number(currentValue);
+          break;
+        case "/":
+          display.innerText = Number(acc) / Number(currentValue);
+          break;
+      }
+      acc = display.innerText;
+      isDisplayActive = false;
+    } else if (keyValue === "CE") {
+      isDisplayActive = false;
+      display.innerText = "0";
+    } else if (keyValue === "C") {
+      isDisplayActive = false;
+      display.innerText = "0";
+      acc = "";
+      currentValue = "";
+      operator = "";
     }
   });
 });
