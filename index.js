a5ebdf4df33020b03218364686f289ef2f7ec1f9
Will Delorm <willdelorm@gmail.com>
2024-01-07 10:47:01 -0800

    Refactor for better readability

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ const output = document.getElementById("output");
 const operands = document.querySelectorAll("button[data-type=operand]");
 const operators = document.querySelectorAll("button[data-type=operator]");
 const clearBtn = document.querySelector("button[data-type=clear]");
-console.log(clearBtn);
 
 let isOperator = false;
+
+const removeActive = () => {
+  operators.forEach((btn) => btn.classList.remove("active"));
+};
+
 operands.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     removeActive();
-    if (output.value == "0") {
-      output.value = e.target.value;
-    } else if (isOperator) {
+    if (output.value == "0" || isOperator) {
       isOperator = false;
       output.value = e.target.value;
     } else if (output.value.includes(".")) {
-      output.value = output.value + "" + e.target.value.replace(".", "");
+      output.value += e.target.value.replace(".", "");
     } else {
-      output.value = output.value + "" + e.target.value;
+      output.value += e.target.value;
     }
     clearBtn.textContent = "C";
   });
@@ -33,10 +35,10 @@ operators.forEach((btn) => {
 
     switch (e.target.value) {
       case "%":
-        output.value = parseFloat(output.value) / 100;
+        output.value /= 100;
         break;
       case "invert":
-        output.value = parseFloat(output.value) * -1;
+        output.value *= -1;
         break;
       case "=":
         equation.push(output.value);
@@ -47,20 +49,13 @@ operators.forEach((btn) => {
         let lastItem = equation[equation.length - 1];
         if (["/", "*", "+", "-"].includes(lastItem) && isOperator) {
           equation.pop();
-          equation.push(e.target.value);
-        } else {
-          equation.push(output.value);
-          equation.push(e.target.value);
         }
+        equation.push(output.value, e.target.value);
         isOperator = true;
         break;
     }
   });
 });
-
-const removeActive = () => {
-  operators.forEach((btn) => btn.classList.remove("active"));
-};
 
 clearBtn.addEventListener("click", (e) => {
   output.value = "0";
